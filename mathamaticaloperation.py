ef394973c92e348af28f83fe4a202d8e19f79e9c
Seenivasan24 <seenivasanofficial24@gmail.com>
2025-09-28 12:30:27 +0530

    filE name changed

---
+++ mathamaticaloperation.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+arr1=np.array([[100,200,300,400,500],[600,700,800,900,100]])
+arr2=np.array([[600,700,800,900,100],[100,200,300,400,500]])
+
+
+#addition two dimensional
+
+print(arr1+arr2)
+
+
+
+
+arr3=np.array([[100,200,300,400,500]])
+arr4=np.array([[600,700,800,900,100],[100,200,300,400,500]])
+
+
+#addition for one dimension and two dimension
+
+print(arr3+arr4)
+
+
+#improper addition 
+
+arr3=np.array([100,200,300,400])
+arr4=np.array([[600,700,800,900,100],[100,200,300,400,500]])
+
+
+# multplication
+
+print(arr1*arr2)
+
+
+#division
+
+
+print(arr1/arr2)
+
+
+#subtraction
+
+
+print(arr1-arr2)
+
+
+
+
+
